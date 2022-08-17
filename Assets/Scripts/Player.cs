237d3d4792c4c6e6ac2c24538b95a1ce159d5190
Guiny-Time <1084926982@qq.com>
2022-08-17 18:22:55 +0800

    grab event, button event( sing song)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,19 +2,28 @@ using System;
 using System.Collections;
 using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace hackathon
 {
     public class Player : MonoBehaviour
     {
+        public AudioSource voice;
+        public AudioSource footstep;
+        [SerializeField] private InputActionReference singSongInputAction;
         private ObstaclesChecker checker;
         float distance;
+        const float voice_speed = 100;      // 游戏里的音速（非物理）
+        
 
         // Start is called before the first frame update
         void Start()
         {
             checker = new ObstaclesChecker();
+            singSongInputAction.action.performed += Sing;
             distance = 0;
+            voice.Stop();
+            footstep.Stop();
         }
 
         // Update is called once per frame
@@ -26,6 +35,11 @@ namespace hackathon
             }else {
                 Debug.Log("Not hit");
             }
+        }
+
+        private void Sing(InputAction.CallbackContext obj)
+        {
+            voice.Play();
         }
     }
 } // namespace hackathon
